7d996beab6dadc868d4bffdfbdc5643b1571c661
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-19 20:47:11 +0800

    Delete employee directory

--- employee/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$conn = mysqli_connect($servername, $username, $password);
-mysqli_select_db($conn, '353p' );
-?>
--- employee/top_navigation.php
+++
@@ -1,9 +0,0 @@
-<div class="top_nav">
-    <div class="nav_menu">
-        <nav>
-            <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-            </div>
-        </nav>
-    </div>
-</div>
